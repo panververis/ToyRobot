463ab7b27ce9c5dbe5d13d9bc2956346adbd969c
panververis <panververis@gmail.com>
2018-10-28 08:50:57 +0000

    Add project files.

---
+++ LittleSpeedyToyRobot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LittleSpeedyToyRobot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
