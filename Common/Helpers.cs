76ba2a58a94cf2ace808ea5e8cc71ac9cc3c54dd
panververis <panververis@gmail.com>
2018-10-28 10:32:13 +0000

    Moved all of the "Common" functionality and "Initial" test parameters into their own "Common" project, so they can be easily and safely referenced by the Speedy console project, as well as from the Tests project

---
+++ Common/Helpers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    /// <summary>
+    /// Public static STRICTLY STATELESS "helper" class, providing some helper public static functions
+    /// </summary>
+    public static class Helpers
+    {
+        /// <summary>
+        /// Public static helper method checking whether the provided position is a valid one
+        /// (namely, if it falls within the range of the valid positions, or if it describes a position that will lead Little Speedy off the surface)
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsPositionValid(int proposedXPosition, int proposedYPosition)
+        {
+            bool isValid = true;
+            if (        proposedXPosition < 0 
+                     || proposedXPosition > InitialParams.TableWidth - 1
+                     || proposedYPosition < 0
+                     || proposedXPosition > InitialParams.TableLength - 1)
+            {
+                isValid = false;
+            }
+            return isValid;
+        }
+    }
+}
---
+++ Common/InitialParams.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    /// <summary>
+    /// Public static class holding the Tech Test's initial parameters (table width and length, for example)
+    /// </summary>
+    public static class InitialParams
+    {
+        /// <summary>
+        /// Denotes the range of the X positions that Speedy can move into (so the allowed positions range from "0" to "TableWidth-1")
+        /// </summary>
+        public static int TableWidth => 5;
+
+        /// <summary>
+        /// Denotes the range of the Y positions that Speedy can move into (so the allowed positions range from "0" to "TableLength-1")
+        /// </summary>
+        public static int TableLength => 5;
+    }
+}
